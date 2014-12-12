fd13373c95150d9d2d50800abb34b77c3f0f2df0
Manfred <FlatheadV8@users.noreply.github.com>
2014-12-12 01:37:27 +0100

    Update commerzbank-csv2moneyplex.sh
    Leider hat das vorher versuchte Format nicht funktioniert.
    Jetzt gibt es hier das Format, wie es laut Matric-Wiki von der VR-NetWorld exportiert werden soll.
    Um den Vorsatz "DB" durch "Fahrtkosten" zu ersetzen, wurde jetzt ein Kategorie-Schalter eingebaut:
    commerzbank-csv2moneyplex.sh -k DB/Fahrtkosten Commerzbank_Kreditkartenabrechnung.csv

--- commerzbank-csv2moneyplex.sh
+++ commerzbank-csv2moneyplex.sh
@@ -11,24 +11,92 @@
 if [ -z "${1}" ] ; then
 	echo "${0} Kreditkartenabrechnung_der_Commerzbank.csv"
 	exit 1
-else
-	CSVDATEI="${1}"
-	NEUERNAME="$(echo "${1}" | sed 's/[( )][( )]*/_/g' | rev | sed 's/.*[.]//' | rev)"
 fi
 
 
 #==============================================================================#
 ### CSV für MoneyPlex umsortieren
 
-# Kategorie-Schalter: DB -> Fahrtkosten
+#------------------------------------------------------------------------------#
+### Kategorie-Schalter: DB -> Fahrtkosten
 
-(echo '"Saldo";"SdoWaehr";"AgBlz";"AgKto";"AgName1";"Storno";"OrigBtg";"Betrag";"BtgWaehr";"OCMTBetr";"OCMTWaehr";"Textschl";"VWZ1";"VWZ2";"VWZ3";"VWZ4";"VWZ5";"VWZ6";"VWZ7";"VWZ8";"VWZ9";"VWZ10";"VWZ11";"VWZ12";"VWZ13";"VWZ14";"BuchDatum";"WertDatum";"Primanota";"Kategorie";"Unterkat";"Kostenst"';
-cat "${CSVDATEI}" | grep -Ev '^Bahnkart;' | grep -Ev '^$' | awk -F';' '{ print ";;;;"$3";;"$9";"$9";EUR;"$5";"$6";"$7";"$4";;;;;;;;;;;;;;"$8";"$2";;"$1";;"$10";" }') > ${NEUERNAME}_moneyplex.csv
+while [ "${#}" -ne "0" ]; do
+        case "${1}" in
+                -k)
+                        KATEGORIE_PARAMETER="${2}"
+                        shift
+                        shift
+                        ;;
+                -h)
+                        echo "
+                        HILFE:
+                        ${0} -k [Kategorie_alt/Kategorie_neu]
+                        ${0} -k DB/Fahrtkosten
+                        "
+                        exit 1
+                        ;;
+                *)
+                        CSVDATEI="${1}"
+                        shift
+                        ;;
+        esac
+done
 
-ls -lh ${NEUERNAME}_moneyplex.txt
+KATEGORIE_ALT="$(echo "${KATEGORIE_PARAMETER}" | awk -F'/' '{print $1}')"
+KATEGORIE_NEU="$(echo "${KATEGORIE_PARAMETER}" | awk -F'/' '{print $2}')"
+NEUERNAME="$(echo "${CSVDATEI}" | sed 's/[( )][( )]*/_/g' | rev | sed 's/.*[.]//' | rev)"
 
+### nur für Tests
+#echo "
+#CSVDATEI='${CSVDATEI}'
+#KATEGORIE_PARAMETER='${KATEGORIE_PARAMETER}'
+#KATEGORIE_ALT='${KATEGORIE_ALT}'
+#KATEGORIE_NEU='${KATEGORIE_NEU}'
+#"
+#------------------------------------------------------------------------------#
+
+
+### MoneyPlex - Import-Format (Fehlerhaft)
+#(echo '"Saldo";"SdoWaehr";"AgBlz";"AgKto";"AgName1";"Storno";"OrigBtg";"Betrag";"BtgWaehr";"OCMTBetr";"OCMTWaehr";"Textschl";"VWZ1";"VWZ2";"VWZ3";"VWZ4";"VWZ5";"VWZ6";"VWZ7";"VWZ8";"VWZ9";"VWZ10";"VWZ11";"VWZ12";"VWZ13";"VWZ14";"BuchDatum";"WertDatum";"Primanota";"Kategorie";"Unterkat";"Kostenst"';
+#cat "${CSVDATEI}" | grep -Ev '^Bahnkart;' | grep -Ev '^$' | while read ZEILE
+#do
+#	if [ -n "${KATEGORIE_PARAMETER}" ] ; then
+#		echo "${ZEILE}" | sed "s/^${KATEGORIE_ALT};/${KATEGORIE_NEU};/" | tee -a /tmp/test.csv
+#	else
+#		echo "${ZEILE}"
+#	fi
+#done | awk -F';' '{ print ";;;;"$3";;"$9";"$9";EUR;"$5";"$6";"$7";"$4";;;;;;;;;;;;;;"$8";"$2";;"$1";;"$10";" }') > ${NEUERNAME}_moneyplex.csv
+
+
+### VR-NetWorld - Export-Format (http://wiki.matrica.com/index.php/Daten%C3%BCbernahme)
+(echo 'Datum;Valuta;Zahlungspflichtiger/-empfänger;ZP/ZEKonto/IBAN;ZP/ZEBankleitzahl/BIC;Verwendungszweck;Kategorie;Betrag;Währung';
+cat "${CSVDATEI}" | grep -Ev '^Bahnkart;' | grep -Ev '^$' | while read ZEILE
+do
+	if [ -n "${KATEGORIE_PARAMETER}" ] ; then
+		echo "${ZEILE}" | sed "s/^${KATEGORIE_ALT};/${KATEGORIE_NEU};/" | tee -a /tmp/test.csv
+	else
+		echo "${ZEILE}"
+	fi
+done | awk -F';' '{ print $2";"$8";"$3,$4";;;"$5,$6,$7";"$1";"$9";EUR" }') > ${NEUERNAME}_moneyplex.csv
+
+# Datum ; Valuta ; Zahlungspflichtiger/-empfänger ; ZP/ZE Konto/IBAN ; ZP/ZE Bankleitzahl/BIC ; Verwendungszweck ; Kategorie ; Betrag ; Währung
+#
+# 01 - Bahnkart		# Kategorie
+# 02 - Umsatzdatum	# Datum
+# 03 - Unternehmen	# Zahlungspflichtiger/-empfänger
+# 04 - Ort		# Zahlungspflichtiger/-empfänger
+# 05 - Ausgabe		# Verwendungszweck
+# 06 - Währung		# Verwendungszweck
+# 07 - Kurs		# Verwendungszweck
+# 08 - Buchungsdatum	# Valuta
+# 09 - Betrag in EUR	# Betrag
+# 10 - Guthaben		# 
+
+ls -lh ${NEUERNAME}_moneyplex.csv
+
+exit
 #==============================================================================#
-
+#
 # "Saldo";"SdoWaehr";"AgBlz";"AgKto";"AgName1";"Storno";"OrigBtg";"Betrag";"BtgWaehr";"OCMTBetr";"OCMTWaehr";"Textschl";"VWZ1";"VWZ2";"VWZ3";"VWZ4";"VWZ5";"VWZ6";"VWZ7";"VWZ8";"VWZ9";"VWZ10";"VWZ11";"VWZ12";"VWZ13";"VWZ14";"BuchDatum";"WertDatum";"Primanota";"Kategorie";"Unterkat";"Kostenst"
 #
 # 63,39;"EUR";"";"";"TESTAUFTRAGGEBER";0;-26,93;-26,93;"EUR";-26,93;"EUR";5;"";"TESTZWECK";"";"";"";"";"";"";"";"";"";"";"";"";17.3.2004;17.3.2004;"9044";"";"";""
